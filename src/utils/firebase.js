64994fb34880021debde2b3fe8d84f725affc61d
Utsav Goyal <utsav28feb@gmail.com>
2026-03-28 19:13:39 +0530

    Security fix: Move API keys and configuration to environment variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHucpSr7x0c2u-cuGClcLwyk5mGcgq-Q8",
-  authDomain: "netflixgpt-b9d89.firebaseapp.com",
-  projectId: "netflixgpt-b9d89",
-  storageBucket: "netflixgpt-b9d89.firebasestorage.app",
-  messagingSenderId: "444080597792",
-  appId: "1:444080597792:web:dcb4649e633000d975c545",
-  measurementId: "G-N8KWS6TC4Z",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
